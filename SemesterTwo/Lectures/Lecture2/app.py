2ef773ef10c00ea9638edd6911055e940f1ea52a
JavaJabba <36043808+JavaJabba@users.noreply.github.com>
2022-01-25 12:43:01 +0000

    week2

---
+++ SemesterTwo/Lectures/Lecture2/app.py
@@ -0,0 +1,17 @@
+from pipes import Template
+from re import template
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/greet_by_name/<name>")
+def send_greeting_by_name(name):
+    return render_template("greet1.html", name=name)
+
+#Jinja2 Template - Html with placeholders.
+
+@app.route("/greet_across_the_world/<language>")
+def send_greeting_in_language(language):
+    hello_dict = {"en":"Hello", "fr":"Bonjour", "es":"Hola"}
+    greeting = hello_dict.get(language, "Hi")
+    return render_template("greet2.html", greeting = greeting)
