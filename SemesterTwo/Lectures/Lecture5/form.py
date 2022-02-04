a3edb5f03a11c2ee8699f6dafd2c22e01b0a77f8
JavaJabba <36043808+JavaJabba@users.noreply.github.com>
2022-02-04 13:57:08 +0000

    more validation

--- SemesterTwo/Lectures/Lecture5/form.py
+++ SemesterTwo/Lectures/Lecture5/form.py
@@ -1,10 +1,11 @@
 from unicodedata import decimal
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
 from wtforms import DecimalField, SubmitField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, NumberRange
 
 class BMIForm(FlaskForm):
-    weight = DecimalField("Weight in kg:", validators=[InputRequired])
-    height = DecimalField("Height in metres:", validators=[InputRequired])
+    weight = DecimalField("Weight in kg:", validators=[InputRequired(), NumberRange(10, 100)])
+    height = DecimalField("Height in metres:", validators=[InputRequired(), NumberRange(0.5, 3)])
     bmi = DecimalField ("BMI:")
     submit = SubmitField("Submit")
