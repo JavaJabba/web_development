246f649297fa7f4ad2dab39fe0780b6be47be4e0
JavaJabba <36043808+JavaJabba@users.noreply.github.com>
2022-03-03 16:43:00 +0000

    lab 05

--- SemesterTwo/Lab/lab05/app.py
+++ SemesterTwo/Lab/lab05/app.py
@@ -1,18 +1,28 @@
+from random import randint
 from flask import Flask, request, render_template, session
 from forms import numberForm
 from flask_session import Session
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "secret_key"
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 @app.route("/guess", methods=["GET", "POST"])
-def guessNumber():
+def guess():
     form = numberForm()
     userNumber = form.userNumber.data
-    if "friendNumber" not in session:
-        session["friendNumber"] = False    
-
-
+    if request.method == "GET":
+        if "secretNumber" not in session:
+            session["secretNumber"] = randint(1, 100)
+            return render_template("number.html", form=form, secretNumber=session["secretNumber"], userNumber=userNumber)
+    if userNumber == session["secretNumber"]:
+        message = "You've guessed it!"
+    else:
+        message = "You've guessed wrong!"
+    return render_template("number.html", form=form, secretNumber=session["secretNumber"], userNumber=userNumber, message=message)
+        
+        
+             
 
