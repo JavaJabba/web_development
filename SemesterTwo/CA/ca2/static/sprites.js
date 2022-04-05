e88a3abb138af0fa1b86769decd9b18b6ed0a89a
JavaJabba <36043808+JavaJabba@users.noreply.github.com>
2022-04-05 18:16:00 +0100

    ca work

--- SemesterTwo/CA/ca2/static/sprites.js
+++ SemesterTwo/CA/ca2/static/sprites.js
@@ -36,22 +36,25 @@ let tileSize = 32;
 let player = {
     x : 0,
     y : 0,
-    width : 32,
-    height : 48,
+    width : 38,
+    height : 32,
     frameX : 0,
     frameY : 0,
     xChange : 0,
     yChange : 0,
+    facing: "none"
 };
 
+let enemy = [];
 let score = 0;
 let playerImage = new Image();
+let enemyImage = new Image();
 let moveLeft = false;
 let moveRight = false;
 let moveUp = false;
 let moveDown = false;
 
-let IMAGES = {player: "static/player.png", background: "static/tileset.png"};
+let IMAGES = {player: "static/player.png", background: "static/tileset.png", enemy: "static/enemy1.png"};
 
 document.addEventListener("DOMContentLoaded", init, false);
 
@@ -62,7 +65,8 @@ function init(){
     player.x = canvas.width / 2;
     player.y = canvas.height / 2;
     playerImage.src = "static/player.png";
-    backgroundImage.src = "static/tileset.png"
+    backgroundImage.src = "static/tileset.png";
+    enemyImage.src = "static/enemy1.png";
 
     window.addEventListener("keydown", activate, false);
     window.addEventListener("keyup", deactivate, false);
@@ -96,23 +100,35 @@ function draw() {
 
     //draw player
     context.drawImage(IMAGES.player, player.frameX * player.width, player.frameY * player.height, player.width, player.height, player.x, player.y, player.width, player.height);
-    if ((moveLeft || moveRight) && (moveLeft && moveRight)) {
-        player.frameX = (player.frameX + 1) % 8;
-    }
 
     if (moveLeft) {
-        player.xChange = player.xChange - 2;
+        player.xChange = player.xChange - 1;
         player.frameY = 9;
+        player.frameX = (player.frameX + 1) % 8;
+        player.facing = "left";
     }
     if (moveRight) {
-        player.xChange = player.xChange + 2;
+        player.xChange = player.xChange + 1;
         player.frameY = 1;
+        player.facing = "right";
+        player.frameX = (player.frameX + 1) % 8;
     }
     if (moveUp) {
-        player.yChange = player.yChange + 2;
+        player.yChange = player.yChange - 1;
+        player.facing = "up";
     }
     if (moveDown) {
-        player.yChange = player.yChange - 2;
+        player.yChange = player.yChange + 1;
+        player.facing = "down";
+    }
+
+    // still needs work, maybe different sprite needed for separate N/S movement
+    if ((moveUp || moveDown ) && player.facing === "left") {
+        player.frameY = 9;
+        player.frameX = (player.frameX + 1) % 8;
+    } else if ((moveUp || moveDown) && player.facing === "right"){
+        player.frameY = 1;
+        player.frameX = (player.frameX + 1) % 8;
     }
 
     //update player
@@ -120,6 +136,52 @@ function draw() {
     player.y = player.y + player.yChange
 
     //Physics
+    player.yChange = player.yChange * 0.9;
+    player.xChange = player.xChange * 0.9;
+
+    // Going off screen
+    if (player.x + player.width < 0){
+        player.x = canvas.width;
+    } else if (player.x > canvas.width){
+        player.x = -player.width;
+    } else if (player.y + player.height < 0){
+        player.y = canvas.height;
+    } else if (player.y > canvas.height){
+        player.y = -player.height;
+    }
+
+
+    //enemy
+    if (enemy.length < 30) {
+        let e = {
+            x : randint(0, canvas.width),
+            y : randint(0, canvas.height),
+            height : 32,
+            width : 32,
+            xChange : player.xChange/2,
+            yChange : player.yChange/2,
+        };
+        enemy.push(e);
+        context.drawImage(IMAGES.enemy, e.x, e.y);
+    
+        for (let e of enemy) {
+            e.x = player.x + e.xChange;
+            e.y = player.y + e.yChange;
+            e.yChange = e.yChange * 0.9;
+            e.xChange = e.xChange * 0.9;
+
+            if (e.x + e.width < 0){
+                e.x = canvas.width;
+            } else if (e.x > canvas.width){
+                e.x = -e.width;
+            } else if (e.y + e.height < 0){
+                e.y = canvas.height;
+            } else if (e.y > canvas.height){
+                e.y = -e.height;
+            }
+        }
+    }
+    
 
     // Score
     score = score + 1
@@ -149,6 +211,10 @@ function deactivate(event){
     } else if (key === "s") {
         moveDown = false;
     }
+}
+
+function randint(min, max) {
+    return Math.round(Math.random() * (max - min)) + min;
 }
 
 function stop(outcome) {
